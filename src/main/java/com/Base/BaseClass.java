f9d06b651131da5156fa31dd04875425425d15d2
sayedluhoque <saeedsumon@gmail.com>
2021-12-19 00:23:34 +0600

    Chrome, Edge, Opera

--- src/main/java/com/Base/BaseClass.java
+++ src/main/java/com/Base/BaseClass.java
@@ -3,15 +3,18 @@ package com.Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseClass {
     public static WebDriver driver;
     public static void main(String[] args) {
-        chrome_launch();
-        open_URL();
-        chrome_close();
+        //chrome_launch();
+        //open_URL();
+        //chrome_close();
         //edge_launch();
        // edge_close();
+        opera_launch();
+        opera_close();
     }
     public static void chrome_launch(){
         System.setProperty("webdriver.chrome.driver","./src/main/resources/Drivers/chromedriver.exe");
@@ -21,6 +24,10 @@ public class BaseClass {
         System.setProperty("webdriver.edge.driver", "./src/main/resources/Drivers/msedgedriver.exe");
         driver = new EdgeDriver();
     }
+    public static void opera_launch() {
+        System.setProperty("webdriver.opera.driver", "./src/main/resources/Drivers/operadriver.exe");
+        driver = new OperaDriver();
+    }
     public static void chrome_close(){
        // driver.close();
         driver.quit();
@@ -28,6 +35,10 @@ public class BaseClass {
     public static void edge_close(){
         //driver.close();
         driver.quit();
+    }
+    public static void opera_close() {
+        driver.close();
+        //driver.quit();
     }
     public static void open_URL(){
         driver.get("https://google.com");
